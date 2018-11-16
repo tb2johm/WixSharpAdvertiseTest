4835b9e30d96840ec6a022eed2b72065caa87e08
Markus Johansson <markus.johansson@semcon.com>
2018-11-16 15:27:46 +0100

    Initial commit

---
+++ WixSharpSetup/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WixSharp;
+
+namespace WixSharpSetup
+{
+    class Program
+    {
+        static void Main()
+        {
+            var project = new Project("FooBar",
+                             new Dir(@"%ProgramFiles%\foo\bar",
+                                 new File(@"..\..\..\FooBar\bin\debug\foobar.exe",
+                                     new FileShortcut("FooBar", @"%ProgramMenu%\FooBar") {WorkingDirectory = "[INSTALLDIR]", Advertise = true})
+                            ));
+
+            project.GUID = new Guid("{43A6B2F2-0212-484D-901B-ACC3C812569A}");
+            project.OutDir = "../..";
+            project.UI = WUI.WixUI_ProgressOnly;
+
+            project.BuildMsi();
+        }
+    }
+}
